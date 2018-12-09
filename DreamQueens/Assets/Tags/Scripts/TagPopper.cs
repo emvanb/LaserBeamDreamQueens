7bd56f839e5ea91dd8a69343fab3da31fcf166fe
Jenna Velez <jennavee@Jennas-MacBook-Pro.local>
2018-12-09 12:21:42 -0800

    pop and sounds quelled like peasant scum

--- DreamQueens/Assets/Tags/Scripts/TagPopper.cs
+++ DreamQueens/Assets/Tags/Scripts/TagPopper.cs
@@ -26,7 +26,7 @@ public class TagPopper : MonoBehaviour {
         
         if(!popOn)
         {
-            transform.localScale = stSc / offset;
+            //transform.localScale = stSc / offset;
         }
         else if(popOn && !isSelected)
         {
@@ -36,28 +36,29 @@ public class TagPopper : MonoBehaviour {
 
     IEnumerator PopOn()
     {
-        transform.localScale = stSc;
-        Vector3 dest = stSc * offset*1.1f;
-        while(Vector3.Distance(transform.localScale, dest)>.1f)
-        {
-            transform.localScale += Vector3.one*speed * Time.deltaTime;
-            yield return null;
-        }
-        while (Vector3.Distance(transform.localScale, stSc * offset) > .1f)
-        {
-            transform.localScale -= Vector3.one * speed *2* Time.deltaTime;
-            yield return null;
-        }
+        //transform.localScale = stSc;
+        //Vector3 dest = stSc * offset*1.1f;
+        //while(Vector3.Distance(transform.localScale, dest)>.1f)
+        //{
+        //    transform.localScale += Vector3.one*speed * Time.deltaTime;
+        //    yield return null;
+        //}
+        //while (Vector3.Distance(transform.localScale, stSc * offset) > .1f)
+        //{
+        //    transform.localScale -= Vector3.one * speed *2* Time.deltaTime;
+        //    yield return null;
+        //}
 
-        src.PlayOneShot(sfx[0]);
-        transform.localScale = stSc * offset;
+        //src.PlayOneShot(sfx[0]);
+        //transform.localScale = stSc * offset;
         isSelected = true;
         GetComponent<TagController>().SetState(true);
+        yield return null;
     }
 
     public void Contract()
     {
-        StopAllCoroutines();
+        //StopAllCoroutines();
         transform.localScale = stSc;
         isSelected = false;
         GetComponent<TagController>().SetState(false);
