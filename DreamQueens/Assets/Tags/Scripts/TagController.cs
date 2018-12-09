9d949c8c0a016105cff6f94aa00664443cd4d3f7
sally.slade <sallyslade@gmail.com>
2018-12-09 11:07:40 -0800

    rotational position wip

--- DreamQueens/Assets/Tags/Scripts/TagController.cs
+++ DreamQueens/Assets/Tags/Scripts/TagController.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 public class TagController : HTags {
 
     private TagPopper tagPopper;
+    private Quaternion myOffset;
 
     private void Start()
     {
         tagPopper = GetComponent<TagPopper>();
+        myOffset = Quaternion.AngleAxis(Random.RandomRange(-30f, 30f), Vector3.up);
 
-        var rot = Quaternion.LookRotation(transform.position- Camera.main.transform.position, Vector3.up);
+        transform.localRotation = GetTargetRotation();
+    }
+
+    private Quaternion GetTargetRotation()
+    {
+        var rot = Quaternion.LookRotation(transform.position - Camera.main.transform.position, Vector3.up);
         rot = ExtractYaw(rot);
-        transform.localRotation = rot * Quaternion.AngleAxis(Random.RandomRange(-30f, 30f), Vector3.up);
+        if (tagPopper.isSelected)
+        {
+            return rot;
+        }
+        return rot * myOffset;
     }
 
     private void Update()
     {
-        if (tagPopper.isSelected)
-        {
-
-        }
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, GetTargetRotation(), Time.deltaTime);
     }
 
     private Quaternion ExtractYaw(Quaternion q)
