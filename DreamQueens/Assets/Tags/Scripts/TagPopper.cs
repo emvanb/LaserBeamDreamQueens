1f4517878bb862ac9c5a01cdaa715837f82e98f5
sally.slade <sallyslade@gmail.com>
2018-12-09 12:28:00 -0800

    updating jenna's tag popper

--- DreamQueens/Assets/Tags/Scripts/TagPopper.cs
+++ DreamQueens/Assets/Tags/Scripts/TagPopper.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TagPopper : MonoBehaviour {
+public class TagPopper : MonoBehaviour
+{
 
 
     public float offset;
@@ -11,24 +12,25 @@ public class TagPopper : MonoBehaviour {
 
     public bool isSelected;
 
-	public AudioClip[] sfx;
+    public AudioClip[] sfx;
     AudioSource src;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         stSc = transform.localScale;
         src = GetComponent<AudioSource>();
-		
-	}
+
+    }
 
     public void Pop(bool popOn)
     {
-        
-        if(!popOn)
+
+        if (!popOn)
         {
-            transform.localScale = stSc / offset;
+            //transform.localScale = stSc / offset;
         }
-        else if(popOn && !isSelected)
+        else if (popOn && !isSelected)
         {
             StartCoroutine(PopOn());
         }
@@ -36,29 +38,30 @@ public class TagPopper : MonoBehaviour {
 
     IEnumerator PopOn()
     {
-        transform.localScale = stSc;
-        Vector3 dest = stSc * offset*1.1f;
-        while(Vector3.Distance(transform.localScale, dest)>.1f)
-        {
-            transform.localScale += Vector3.one*speed * Time.deltaTime;
-            yield return null;
-        }
-        while (Vector3.Distance(transform.localScale, stSc * offset) > .1f)
-        {
-            transform.localScale -= Vector3.one * speed *2* Time.deltaTime;
-            yield return null;
-        }
+        //transform.localScale = stSc;
+        //Vector3 dest = stSc * offset*1.1f;
+        //while(Vector3.Distance(transform.localScale, dest)>.1f)
+        //{
+        //    transform.localScale += Vector3.one*speed * Time.deltaTime;
+        //    yield return null;
+        //}
+        //while (Vector3.Distance(transform.localScale, stSc * offset) > .1f)
+        //{
+        //    transform.localScale -= Vector3.one * speed *2* Time.deltaTime;
+        //    yield return null;
+        //}
 
-        src.PlayOneShot(sfx[0]);
-        transform.localScale = stSc * offset;
+        //src.PlayOneShot(sfx[0]);
+        //transform.localScale = stSc * offset;
         isSelected = true;
         GetComponent<TagController>().SetState(true);
+        yield return null;
     }
 
     public void Contract()
     {
-        StopAllCoroutines();
-        transform.localScale = stSc;
+        //StopAllCoroutines();
+        //transform.localScale = stSc;
         isSelected = false;
         GetComponent<TagController>().SetState(false);
     }
